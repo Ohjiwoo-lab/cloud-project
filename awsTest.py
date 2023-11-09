4c148b6aa5c9a4c6c02fa14f889566e9d54bb18a
Ohjiwoo <jiu3159@naver.com>
2023-11-09 22:54:03 +0900

    feature: 가용영역, 리전 출력하기 구현 (#1)

--- awsTest.py
+++ awsTest.py
@@ -19,27 +19,56 @@ class Instance:
 
     # 특정 인스턴스 시작
     def start(self, id):
-        print(f"Starting .... {id}")
-
+        flag = False
         for instance in self.ec2.instances.all():
             if instance.id == id:
-                response = instance.start()
-                print(response)
-                instance.wait_until_running()
+                print(f"Starting .... {id}")
+                instance.start()
+                flag = True
+                # instance.wait_until_running()
                 break
-
-        print(f"Successfully started instance {id}")
+        if flag:
+            print(f"Successfully started instance {id}")
+        else:
+            print("Incorrect Id. Please try again.")
 
     # 특정 인스턴스 중지
     def stop(self, id):
+        flag = False
         for instance in self.ec2.instances.all():
             if instance.id == id:
-                response = instance.stop()
-                print(response)
-                instance.wait_until_stopped()
+                instance.stop()
+                flag = True
+                # instance.wait_until_stopped()
                 break
 
-        print(f"Successfully stop instance {id}")
+        if flag:
+            print(f"Successfully stop instance {id}")
+        else:
+            print("Incorrect Id. Please try again.")
+
+class Region:
+    def __init__(self, client):
+        self.client = client
+
+    # 가용 영역 출력하기
+    def get_availability_zone(self):
+        response = self.client.describe_availability_zones()
+        cnt=0
+        for zone in response['AvailabilityZones']:
+            print(f"[id] {zone['ZoneId']}, ", end="")
+            print(f"[region] {zone['RegionName']}, ", end="")
+            print(f"[zone] {zone['ZoneName']}")
+            cnt += 1
+
+        print(f"You have access to {cnt} Availability Zones.")
+
+    # 리전 출력하기
+    def get_region(self):
+        response = self.client.describe_regions()
+        for region in response['Regions']:
+            print(f"[region] {region['RegionName']}, ", end="")
+            print(f"[endpoint] {region['Endpoint']}")
 
 
 if __name__ == '__main__':
@@ -60,34 +89,47 @@ if __name__ == '__main__':
 
         ec2 = boto3.resource('ec2')
         instance = Instance(ec2)
+
+        client = boto3.client('ec2')
+        region = Region(client)
+
+        # 인스턴스 목록 출력
         if operation=='1':
             instance.display()
 
+        # 가용영역 출력
         elif operation=='2':
-            pass
+            region.get_availability_zone()
 
+        # 특정 인스턴스 시작
         elif operation=='3':
             id = input("Enter instance id: ")
             if id is not None:
                 instance.start(id)
 
+        # 리전 출력
         elif operation=='4':
-            pass
+            region.get_region()
 
+        # 특정 인스턴스 종료
         elif operation=='5':
             id = input("Enter instance id: ")
             if id is not None:
                 instance.stop(id)
 
+        # 인스턴스 생성
         elif operation=='6':
             pass
 
+        # 인스턴스 재부팅
         elif operation=='7':
             pass
 
+        # AMI 이미지 목록 출력
         elif operation=='8':
             pass
 
+        # 프로그램 종료
         elif operation=='99':
             break
 
